4a04a3257a042d9fd8896416b471e42555381b5a
shem <shem@svcollege.co.il>
2020-06-10 20:13:15 +0300

    Updates

---
+++ precache-manifest.f12492791923ebb7fb0c95391c35d8ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5bf61270289c753c313bfae9fd91908",
+    "url": "/testWevsite/index.html"
+  },
+  {
+    "revision": "c3ce5ba9b7c7234e9cb0",
+    "url": "/testWevsite/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "37a9b4254d4d8e035c42",
+    "url": "/testWevsite/static/js/2.e2d2f896.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/testWevsite/static/js/2.e2d2f896.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3ce5ba9b7c7234e9cb0",
+    "url": "/testWevsite/static/js/main.27f84002.chunk.js"
+  },
+  {
+    "revision": "ec83c87e245dd96db0f7",
+    "url": "/testWevsite/static/js/runtime-main.fea60068.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testWevsite/static/media/logo.5d5d9eef.svg"
+  }
+]);
